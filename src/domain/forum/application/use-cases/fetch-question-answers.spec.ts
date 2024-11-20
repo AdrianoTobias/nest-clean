6864fa619b4da93a537b506100423fe8d3912cfd
Adriano <adrianoact@msn.com>
2024-11-20 16:10:00 -0300

    Relationships and uploads: Answer with author

--- src/domain/forum/application/use-cases/fetch-question-answers.spec.ts
+++ src/domain/forum/application/use-cases/fetch-question-answers.spec.ts
@@ -3,37 +3,49 @@ import { FetchQuestionAnswersUseCase } from '@/domain/forum/application/use-case
 import { makeAnswer } from 'test/factories/make-answer'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { InMemoryAnswerAttachmentsRepository } from 'test/repositories/in-memory-answer-attachments-repository'
+import { InMemoryStudentsRepository } from 'test/repositories/in-memory-students-repository'
+import { makeStudent } from 'test/factories/make-student'
 
 let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository
 let inMemoryAnswersRepository: InMemoryAnswersRepository
+let inMemoryStudentsRepository: InMemoryStudentsRepository
 let sut: FetchQuestionAnswersUseCase
 
 describe('Fetch Question Answers', () => {
   beforeEach(() => {
     inMemoryAnswerAttachmentsRepository =
       new InMemoryAnswerAttachmentsRepository()
+    inMemoryStudentsRepository = new InMemoryStudentsRepository()
     inMemoryAnswersRepository = new InMemoryAnswersRepository(
       inMemoryAnswerAttachmentsRepository,
+      inMemoryStudentsRepository,
     )
     sut = new FetchQuestionAnswersUseCase(inMemoryAnswersRepository)
   })
 
   it('should be able to fetch question answers', async () => {
-    await inMemoryAnswersRepository.create(
-      makeAnswer({
-        questionId: new UniqueEntityID('question-1'),
-      }),
-    )
-    await inMemoryAnswersRepository.create(
-      makeAnswer({
-        questionId: new UniqueEntityID('question-1'),
-      }),
-    )
-    await inMemoryAnswersRepository.create(
-      makeAnswer({
-        questionId: new UniqueEntityID('question-1'),
-      }),
-    )
+    const student = makeStudent({ name: 'John Doe' })
+
+    inMemoryStudentsRepository.items.push(student)
+
+    const answer1 = makeAnswer({
+      questionId: new UniqueEntityID('question-1'),
+      authorId: student.id,
+    })
+
+    const answer2 = makeAnswer({
+      questionId: new UniqueEntityID('question-1'),
+      authorId: student.id,
+    })
+
+    const answer3 = makeAnswer({
+      questionId: new UniqueEntityID('question-1'),
+      authorId: student.id,
+    })
+
+    await inMemoryAnswersRepository.create(answer1)
+    await inMemoryAnswersRepository.create(answer2)
+    await inMemoryAnswersRepository.create(answer3)
 
     const result = await sut.execute({
       questionId: 'question-1',
@@ -41,13 +53,34 @@ describe('Fetch Question Answers', () => {
     })
 
     expect(result.value?.answers).toHaveLength(3)
+    expect(result.value?.answers).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          author: 'John Doe',
+          answerId: answer1.id,
+        }),
+        expect.objectContaining({
+          author: 'John Doe',
+          answerId: answer2.id,
+        }),
+        expect.objectContaining({
+          author: 'John Doe',
+          answerId: answer3.id,
+        }),
+      ]),
+    )
   })
 
   it('should be able to fetch paginated question answers', async () => {
+    const student = makeStudent({ name: 'John Doe' })
+
+    inMemoryStudentsRepository.items.push(student)
+
     for (let i = 1; i <= 22; i++) {
       await inMemoryAnswersRepository.create(
         makeAnswer({
           questionId: new UniqueEntityID('question-1'),
+          authorId: student.id,
         }),
       )
     }
